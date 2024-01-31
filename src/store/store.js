a84e6b7fd6a614a7f01750bcd8ed762bcf88265c
Ankit4454 <kanteliyaankit@gmail.com>
2024-02-01 02:42:31 +0530

    info: Made redux store persistent

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "../reducers/cartReducers";
 import { wishlistReducer } from "../reducers/wishlistReducers";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const cartPersistConfig = {
+    key: 'cart',
+    storage: storage
+};
+
+const wishlistPersistConfig = {
+    key: 'wishlist',
+    storage: storage
+}
+
+const persistedCartReducer = persistReducer(cartPersistConfig, cartReducer);
+const persistedWishlistReducer = persistReducer(wishlistPersistConfig, wishlistReducer);
 
 export const store = configureStore({
-    reducer:{
-        cartReducer,
-        wishlistReducer
+    reducer: {
+        cart: persistedCartReducer,
+        wishlist: persistedWishlistReducer
     }
 });
+
+export const persistor = persistStore(store);
